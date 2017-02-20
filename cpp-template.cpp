19daf6dd60eb0dfa7f8b93c956ed8d28b6a125cc
nj7 <nirmitjain913@gmail.com>
2017-02-20 23:07:11 +0530

    Add files via upload

---
+++ cpp-template.cpp
@@ -0,0 +1,32 @@
+/*This Template is created by Nirmit Jain (github@nj7)*/
+
+//Best header file for competitive programming
+#include<bits/stdc++.h>
+
+//Macro for Data Type
+#define lli long long int
+#define llu unsigned long long int
+#define ld long double
+
+//Macro for scanning basic input
+#define si(n) scanf("%d",&n)
+#define slli(n) scanf("%lld",&n);
+#define ss(n) scanf("%s",n);
+
+
+//Print loop for array element in new line
+void print(int a[],int s,int e){for(int i=s;i<=e;i++)cout<<a[i]<<"\n";}
+void print(vector<int> &v,int s,int e){for(int i=s;i<=e;i++)cout<<v[i]<<"\n";}
+//this for loop only works with c++11 and higher
+void print(vector<int> &v){for(int x:v)cout<<x<<"\n";}
+
+using namespace std;
+
+int main()
+{
+    //for taking input from "input.txt" and storing the output to "output.txt".
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    debug = 1;
+#endif
